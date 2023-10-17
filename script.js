162037a3fd2925cc11d4a3ba49f4a81d295e4605
aalbana <ahmed.albanna98@gmail.com>
2023-10-17 16:25:07 +0700

    add text input, password, number input and text area

---
+++ script.js
@@ -0,0 +1,20 @@
+// Toggle password show and hidden button
+
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleButtons = document.querySelectorAll(".toggle-password");
+    const passwordInputs = document.querySelectorAll(".password-input");
+  
+    toggleButtons.forEach((toggleButton, index) => {
+      toggleButton.addEventListener("click", function () {
+        if (passwordInputs[index].type === "password") {
+          passwordInputs[index].type = "text";
+          toggleButton.classList.remove("bi-eye-slash-fill");
+          toggleButton.classList.add("bi-eye-fill");
+        } else {
+          passwordInputs[index].type = "password";
+          toggleButton.classList.remove("bi-eye-fill");
+          toggleButton.classList.add("bi-eye-slash-fill");
+        }
+      });
+    });
+  });
