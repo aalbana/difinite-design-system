c16f30f92d7998c9363c37f7f32eb797ad66440f
aalbana <ahmed.albanna98@gmail.com>
2023-11-13 13:42:41 +0700

    Revert "before reverting"
    This reverts commit 8c36554819deef2e1362dfd7e0cb36852f175f55.

--- components/datePicker.js
+++
@@ -1,9 +0,0 @@
-export default class DatePicker {
-    constructor(element){
-        this.element.DatePicker = element
-        this.datePickerContainer = document.createElement('div')
-        this.datePickerHeader = document.createElement('div')
-        this.contentContainer = document.createElement('div')
-
-    }
-}
