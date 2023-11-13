8c36554819deef2e1362dfd7e0cb36852f175f55
aalbana <ahmed.albanna98@gmail.com>
2023-11-13 13:42:38 +0700

    before reverting

---
+++ components/datePicker.js
@@ -0,0 +1,9 @@
+export default class DatePicker {
+    constructor(element){
+        this.element.DatePicker = element
+        this.datePickerContainer = document.createElement('div')
+        this.datePickerHeader = document.createElement('div')
+        this.contentContainer = document.createElement('div')
+
+    }
+}
